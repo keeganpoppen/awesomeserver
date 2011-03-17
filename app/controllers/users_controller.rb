6cf1523b05642f230a4640ed2393fb0652b21548
Keegan Poppen <keegan.poppen@gmail.com>
2011-03-17 08:43:02 -0700

    fixed users just a bit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,15 @@ class UsersController < ApplicationController
     respond_to :html, :json
 
     def create
-        @u = User.new
-
 		obj = ActiveSupport::JSON.decode(request.body) 
+
+        @u = User.where(:udid => obj['udid'])
+        
+        if @u.length > 0
+            @u = @u.first
+        else
+            @u = User.new
+        end
 
         @u.udid = obj['udid']
         @u.name = obj['name'] 
